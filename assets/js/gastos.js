5c198b7e93393681aa178ab99af89107922b961e
pablohariq <lizamav.p@gmail.com>
2021-07-27 02:25:35 -0400

    agrega el metodo delete gasto

--- assets/js/gastos.js
+++ assets/js/gastos.js
@@ -1,19 +1,21 @@
 const fs = require('fs')
 const {v4: uuidv4} = require('uuid')
 
-
+//POST gasto
 const agregarGasto = (gasto) => {
     const {gastos} = obtenerGastos()
     gastos.push(gasto)
     fs.writeFileSync('./assets/json/gastos.json', JSON.stringify({gastos: gastos}))
 }
 
+//GET gasto
 const obtenerGastos = () => {
     const gastosJSON = fs.readFileSync('./assets/json/gastos.json', 'utf-8')
     const contenidoArchivoGastos = JSON.parse(gastosJSON)
     return contenidoArchivoGastos
 }
 
+//DELETE gasto
 const borrarGasto = (id) => {
     const gastosJSON = fs.readFileSync('./assets/json/gastos.json', 'utf-8')
     const {gastos: arregloGastos} = JSON.parse(gastosJSON)
@@ -23,4 +25,7 @@ const borrarGasto = (id) => {
     //guardar el nuevo arreglo en el json de gastos
     fs.writeFileSync('./assets/json/gastos.json', JSON.stringify({gastos: arregloGastos}))
 }
+
+//PUT gasto
+
 module.exports = {agregarGasto, obtenerGastos, borrarGasto}
